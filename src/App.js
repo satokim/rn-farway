a9fcab2136c9d93fd3eae70311d1631876357964
sato_yusuke <usksty@gmail.com>
2024-10-22 19:02:25 +0900

    - add : project

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Form from "./components/Form.js";
+import Logo from "./components/Logo.js";
+import PackingList from "./components/PackingList.js";
+import Stats from "./components/Stats.js";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
+];
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
+      <Stats />
+    </div>
+  );
+}
---
+++ src/components/PackingList.js
@@ -0,0 +1,13 @@
+import Item from "./Item";
+
+export default function PackingList({ items }) {
+  return (
+    <div className="list">
+      <ul>
+        {items.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
